63711c43ddba0c0036c31ad7439a2de364ab97b7
Carlo782 <c.negroni01@ufromail.cl>
2023-11-23 05:31:20 -0300

    detalles

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import RequestsView from '../../views/requests/requests-form.jsx'
 import RequestsConfirmView from '../../views/requests/requests-confirmation.jsx'
 import ComplainView from '../../views/complain/complain.jsx'
 import FunctionaryView from '../../views/functionary-views/functionary-view-main.jsx'
+import ShowDetail from '../../views/functionary-views/show-detail.jsx';
 
 export default function AppRoutes() {
     return (
@@ -15,6 +16,7 @@ export default function AppRoutes() {
                 <Route path="/reclamo" element={<ComplainView/>}></Route>
                 <Route path="/funcionario" element={<FunctionaryView/>}></Route>
                 <Route path="/confirmacion-solicitud" element={<RequestsConfirmView/>} />
+                <Route path="/detail" element={<ShowDetail/>} />
             </Routes>
         </Router>
     );
