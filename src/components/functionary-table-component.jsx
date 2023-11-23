de29e5ce715835c6d1a825b90190d518fc0e803b
Belfor Acuña <131080733+belfor-acuna@users.noreply.github.com>
2023-11-23 04:01:28 -0300

    Get complaints into functionary table implementation.

--- src/components/functionary-table-component.jsx
+++ src/components/functionary-table-component.jsx
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
-import { Link } from 'react-router-dom';
-import "./functionary-table-component.css";
+import ComplaintsServices from '../adapters/api/complaints.service';
+import './functionary-table-component.css';
 
 export default function CustomTable() {
+    const [complaints, setComplaints] = useState([]);
+
+    useEffect(() => {
+        async function fetchComplaints() {
+                const complaintsData = await ComplaintsServices.getComplaints();
+                console.log("Complaints data")
+                console.log(complaintsData)
+                setComplaints(complaintsData);
+        }
+        fetchComplaints();
+        console.log(complaints);
+    }, []);
+    
     return (
         <div className="table-container mx-auto mt-4 p-4">
             <Table bordered hover responsive="sm" className="rounded">
                 <thead>
                     <tr>
                         <th>Descripción</th>
-                        <th>Tipo</th>
+                        <th>Categoría</th>
+                        <th>Subcategoría</th>
                         <th>Estado</th>
+                        <th>Rut</th>
                         <th>Detalles</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Asunto 1</td>
-                        <td>Tipo 1</td>
-                        <td>Estado 1</td>
-                        <td>
-                            <Link to="/detalles/1">Detalles 1</Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Asunto 2</td>
-                        <td>Tipo 2</td>
-                        <td>Estado 2</td>
-                        <td>
-                            <Link to="/detalles/2">Detalles 2</Link>
-                        </td>
-                    </tr>
-                    {/* ... Puedes agregar más filas según sea necesario */}
+                    {complaints && complaints.length > 0 ? (
+                        complaints.map((complaint) => (
+                            <tr key={complaint._id}>
+                                <td>{complaint.description}</td>
+                                <td>{complaint.category}</td>
+                                <td>{complaint.subcategory}</td>
+                                <td>{complaint.status}</td>
+                                <td>
+                                   {complaint.userRut}
+                                </td>
+                                <td>
+                                   <a href="#" >Detalle</a>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="5">No hay quejas disponibles.</td>
+                        </tr>
+                    )}
                 </tbody>
             </Table>
         </div>
